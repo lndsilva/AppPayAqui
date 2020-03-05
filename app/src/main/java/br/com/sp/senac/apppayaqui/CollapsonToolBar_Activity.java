03ed7cc5390e42cec940582f626a20352db31972
laercio.nsilva <lndsilva@hotmail.com>
2020-03-05 15:56:26 -0300

    Initial commit

---
+++ app/src/main/java/br/com/sp/senac/apppayaqui/CollapsonToolBar_Activity.java
@@ -0,0 +1,14 @@
+package br.com.sp.senac.apppayaqui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CollapsonToolBar_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.collapson_tool_bar_layot);
+    }
+}
---
+++ app/src/main/java/br/com/sp/senac/apppayaqui/MainActivity.java
@@ -0,0 +1,18 @@
+package br.com.sp.senac.apppayaqui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
